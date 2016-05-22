665132588ad11747db47770c3e0b51e8871199d6
Danny Thai <dthai92@yahoo.com>
2016-05-22 12:55:57 -0700

    refactor fractionConverter

--- fractionConverter.js
+++ fractionConverter.js
@@ -1,52 +1,22 @@
 function fractionConverter (number) {
-  var negative = false;
-
   var denominator = 1;
   var numerator = number;
 
-  if(number < 0) {
-    numerator = Math.abs(numerator);
-    negative = true;
-  }
-
-  if(number % 1 === 0) {
-    if(negative) {
-      return '-' + numerator + '/1';
-    }
-    return numerator + '/1';
-  }
-  // isolate the decimal
-
-  // convert the decimal into a fraction
+  // convert the decimal into integers
   while(numerator % 1 !== 0) {
     numerator = numerator * 10;
     numerator = numerator.toFixed(6);
     denominator = denominator * 10;
   }
 
-  var lcd;
-  // find the lowest common denominator
-  for(var i = numerator; i >= 0; i--) {
-    if(numerator % i === 0 && denominator % i === 0) {
-      lcd = i;
-      break;
+  var divisor = gcd(numerator, denominator);
+
+  function gcd(a, b) {
+    if(!b) {
+      return a;
     }
+    return gcd(b, a % b);
   }
 
-  if(!lcd) {
-    if(negative) {
-      return '-' + numerator + '/' + denominator;
-    }
-    return numerator + '/' + denominator;
-  }
-  var numerator = numerator/lcd;
-  var denominator = denominator/lcd;
-
-  var fraction = numerator + '/' + denominator;
-
-  if(negative) {
-    return '-' + fraction;
-  }
-
-  return fraction;
+  return numerator/divisor + '/' + denominator/divisor;
 }
