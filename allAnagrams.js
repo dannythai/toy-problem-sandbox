4e256e6ba49ea0da90a598d8f220dc6455c35ed7
Danny Thai <dthai92@yahoo.com>
2016-06-01 15:03:32 -0700

    complete allAnagrams.js

--- allAnagrams.js
+++ allAnagrams.js
@@ -1,43 +1,15 @@
-//input string - output: array
-// create an empty array called results
-// create an options variable containing possible options
-// recursive function
-    // input leftover options, current string build
-
-    // loop through input recursive options
-      // select letter and push it to the string build
-      // remove option letter from string using slice
-
-    // check if length of the current string build equals the input string
-      // if yes, push the string into the results array and return
-      // if not then recurse passing in the string build, and leftover options from iteration
-
-// return results array
-
-
-var allAnagrams = function(str) {
-  debugger;
-  var results = [];
-  var optionsArr = str.split('');
-
-  var recurse = function(build, options) {
-    if(build.length === str.length) {
-      build.join('')
-      results.push(build);
+function allAnagrams (str) {
+  var result = [];
+  var recurse = function (curr, strArr) {
+    if(curr.length === str.length) {
+      result.push(curr);
+      return;
     }
-
-    for(var i = 0; i < options.length; i++) {
-      build.push(options[i]);
-      recurse(build, options)
-      // build.push(options[i]);
-      // options.splice(i,1);
-      // recurse(build, options);
+    for(var i = 0; i < strArr.length; i++) {
+      var build = curr + strArr[i];
+      recurse(build, strArr.slice(0, i) + strArr.slice(i+1));
     }
   }
-
-  recurse([], optionsArr);
-
-  return results;
+  recurse('', str);
+  return result;
 }
-
-console.log(allAnagrams("abc"));
